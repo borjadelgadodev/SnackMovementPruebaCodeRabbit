de20b65347575522173cb2682f15ece5d992df1c
Borja Delgado Alcolea <borjadelgadodev@gmail.com>
2025-01-25 20:56:04 +0100

    Test CodeRabbit q

--- app/src/main/java/com/cristiangoncas/snackmovement/ui/notifications/NotificationsViewModel.kt
+++ app/src/main/java/com/cristiangoncas/snackmovement/ui/notifications/NotificationsViewModel.kt
@@ -6,8 +6,13 @@ import androidx.lifecycle.ViewModel
 
 class NotificationsViewModel : ViewModel() {
 
+    val prueba = "CodeRabbit"
+    val prueba2 = "CodeRabbit2"
+
     private val _text = MutableLiveData<String>().apply {
         value = "This is notifications Fragment"
     }
     val text: LiveData<String> = _text
+
+
 }
