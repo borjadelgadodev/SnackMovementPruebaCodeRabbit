4a8ada436afa20390dfbd9488b04f5ca110e69e6
Borja Delgado Alcolea <borjadelgadodev@gmail.com>
2025-01-25 21:28:49 +0100

    Test CodeRabbit 3

--- app/src/main/java/com/cristiangoncas/snackmovement/ui/home/HomeFragment.kt
+++ app/src/main/java/com/cristiangoncas/snackmovement/ui/home/HomeFragment.kt
@@ -13,6 +13,9 @@ class HomeFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
 
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater,
